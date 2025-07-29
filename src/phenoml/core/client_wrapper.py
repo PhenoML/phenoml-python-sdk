3352037129392d22311ed6ab53346c4a530a9a90
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-29 15:28:49 +0000

    SDK regeneration

--- src/phenoml/core/client_wrapper.py
+++ src/phenoml/core/client_wrapper.py
@@ -22,10 +22,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
-            "User-Agent": "phenoml/0.0.2",
+            "User-Agent": "phenoml/0.0.3",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "phenoml",
-            "X-Fern-SDK-Version": "0.0.2",
+            "X-Fern-SDK-Version": "0.0.3",
             **(self.get_custom_headers() or {}),
         }
         headers["Authorization"] = f"Bearer {self._get_token()}"
